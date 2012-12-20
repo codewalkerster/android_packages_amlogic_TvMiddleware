22d42414fc1a8caee40748226a403f9e073cca0e
jiangzhongming <zhongming.jiang@amlogic.com>
2012-12-20 10:21:32 +0800

    all band scan handle

--- src/com/amlogic/tvservice/TVService.java
+++ src/com/amlogic/tvservice/TVService.java
@@ -825,7 +825,7 @@ public class TVService extends Service implements TVConfig.Update{
 			return;
 		}
 		
-		int[] freqList = null;
+		TVChannelParams[] channelList = null;
 		if (sp.getTvMode() != TVScanParams.TV_MODE_ATV &&
 			sp.getDtvMode() == TVScanParams.DTV_MODE_ALLBAND) {
 			String region;
@@ -862,7 +862,7 @@ public class TVService extends Service implements TVConfig.Update{
 			}
 		} 
 		
-		tsp.setDtvParams(0, freqList);
+		tsp.setDtvParams(0, channelList);
 
 		/** No exceptions, start scan */
 		stopPlaying();
