959cc67ee6f21d77426093398e2a62818545d728
ke.gong <ke.gong@amlogic.com>
2014-03-21 10:31:24 +0800

    Pd #71350: Fixed Adnroid.mk.

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-include $(ANDROID_BUILD_TOP)/device/amlogic/$(TARGET_PRODUCT)/BoardConfig.mk
+-include $(ANDROID_BUILD_TOP)/device/amlogic/$(TARGET_PRODUCT)/BoardConfig.mk
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src/com/amlogic/tvutil)
 LOCAL_SRC_FILES += $(call all-java-files-under, src/com/amlogic/tvclient)
